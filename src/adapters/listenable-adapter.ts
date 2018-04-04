ba054c417773ec327b561fa88108142d1b790891
resoliwan <resoliwan@naver.com>
2018-04-04 17:29:36 +0900

    feat: bump up typescript to 2.8

--- src/adapters/listenable-adapter.ts
+++ src/adapters/listenable-adapter.ts
@@ -22,13 +22,13 @@ export interface IListenableAdapter extends IAbstractAdapter {
  * @implements IListenableAdapter
  */
 export default class ListenableAdapter<T, U> extends AbstractAdapter<T, U> implements IListenableAdapter {
-  private _controllersClasses: typeof AbstractController[] = [];
+  private _controllersClasses: {new(...args: any[]): AbstractController<T>;}[] = [];
   private _controllers: AbstractController<T>[] = [];
 
   /**
    * @param {AbstractController} Class
    */
-  public registerController(Class: typeof AbstractController) {
+  public registerController(Class: {new(...args: any[]): AbstractController<T>;}) {
     this._controllersClasses.push(Class);
   }
 
