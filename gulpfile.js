ea52607d4ca9bbb8d0d47c47e41ad90b62c7dcff
kuyh <ppoi001@naver.com>
2018-06-21 15:38:21 +0900

    Feat.bump.up.gulp.4.0 (#235)
    * feat: bump up gulp to 4.0.0
    
    * feat: update base Docker image
    
    * feat: update travis setting
    
    * feat: add gulp-typescript build for incremental build test
    
    * feat: update gulp.watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,43 @@
 var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
 var istanbul = require('gulp-istanbul');
-var sourcemaps = require('gulp-sourcemaps');
 var tslint = require('gulp-tslint');
 var remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul');
 
 var sources = ['./src/**/*.ts'];
 
 function executeIslandDocGen(options) {
-  options = options || {};
-  var islandDoc = require('island-doc').default;
+  if (process.env.npm_lifecycle_event === 'test') {
+    return function (done) { return done(); };
+  }
 
+  options = options || {};
   return function (done) {
+    var islandDoc = require('island-doc').default;
     islandDoc.run(done);
   };
+}
+
+// for incremental build test
+function compileWithGulpTypescript() {
+  var ts = require('gulp-typescript');
+  var sourcemaps = require('gulp-sourcemaps');
+  var tsProject = ts.createProject('tsconfig.json');
+
+  var tsResult = gulp.src('src/**/*.ts', { since: gulp.lastRun('scripts') })
+    .pipe(sourcemaps.init())
+    .pipe(tsProject());
+
+  tsResult.dts.pipe(gulp.dest('dist'));
+  return tsResult.js.pipe(sourcemaps.write()).pipe(gulp.dest('dist'));
 }
 
 function executeTypescriptCompiler(options) {
   options = options || {};
   options.project = options.project || process.cwd();
+
   var command = makeTscCommandString(options);
-  return function (done) {
+  return function compileTypescript(done) {
     require('child_process').exec(command, function (err, stdout, stderr) {
       var outString = stdout.toString();
       if (outString) console.log('\n', outString);
@@ -47,7 +64,7 @@ function makeTscCommandString(options) {
 }
 
 function watch() {
-  gulp.watch(sources, {interval: 2000}, ['buildIgnoreError']);
+  gulp.watch(sources, gulp.series('scripts'));
 }
 
 function clean(done) {
@@ -66,13 +83,14 @@ function registerJasmineTasks() {
 
 function jasmineTask(name) {
   var buildAndTest = 'run-' + name;
-  gulp.task(name, [buildAndTest], function () {
-    // gulp.watch(sources, [buildAndTest]);
-  });
-  gulp.task(buildAndTest, ['build'], function () {
+  gulp.task(buildAndTest, gulp.series(['build'], function () {
     return gulp.src('./dist/spec/' + name + '.js')
       .pipe(jasmine());
-  });
+  }));
+
+  gulp.task(name, gulp.series([buildAndTest], function () {
+    // gulp.watch(sources, [buildAndTest]);
+  }));
 }
 
 function preIstanbulTask() {
@@ -80,14 +98,17 @@ function preIstanbulTask() {
     .pipe(istanbul())
     .pipe(istanbul.hookRequire());
 }
+
 function istanbulTask() {
   const stream = gulp.src(['dist/spec/*.js']).pipe(jasmine());
   // https://github.com/gulpjs/gulp/issues/358 or gulp-plumber
   stream.on('error', (e) => {
+    console.error('error on running coverage: ', e);
     process.exit(1);
   });
   return stream.pipe(istanbul.writeReports());
 }
+
 function remapIstanbulTask() {
   return gulp.src('coverage/coverage-final.json')
     .pipe(remapIstanbul({
@@ -99,7 +120,9 @@ function remapIstanbulTask() {
 }
 
 function doLint() {
-  if (process.env.npm_lifecycle_event === 'test') return;
+  if (process.env.npm_lifecycle_event === 'test') {
+    return gulp.src('empty', { allowEmpty: true });
+  }
   return gulp.src('src/**/*.ts')
     .pipe(tslint({
       fix: true,
@@ -110,14 +133,16 @@ function doLint() {
     }));
 }
 
-gulp.task('env-doc', executeIslandDocGen());
-gulp.task('build', ['tslint', 'env-doc'], executeTypescriptCompiler());
-gulp.task('buildIgnoreError', executeTypescriptCompiler({noEmitOnError: '', taskAlwaysSucceed: true}));
-gulp.task('clean', clean);
-gulp.task('coverage', ['coverage-js'], remapIstanbulTask);
-gulp.task('coverage-js', ['pre-coverage'], istanbulTask);
-gulp.task('pre-coverage', ['build'], preIstanbulTask);
 gulp.task('tslint', doLint);
+gulp.task('env-doc', executeIslandDocGen());
+gulp.task('buildIgnoreError', executeTypescriptCompiler({noEmitOnError: '', taskAlwaysSucceed: true}));
+
+gulp.task('clean', clean);
 gulp.task('watch', watch);
+gulp.task('scripts', compileWithGulpTypescript);
+gulp.task('build', gulp.series(gulp.parallel('tslint', 'env-doc'), executeTypescriptCompiler()));
+gulp.task('pre-coverage', preIstanbulTask);
+gulp.task('coverage-js', istanbulTask);
+gulp.task('coverage', gulp.series('build', 'pre-coverage', 'coverage-js', remapIstanbulTask));
 
 registerJasmineTasks();
